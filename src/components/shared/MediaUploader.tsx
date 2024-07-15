3db20aea3abb17b550c67d25ba439482958769b1
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2024-07-15 15:40:33 +0530

    upload preset folder

--- src/components/shared/MediaUploader.tsx
+++ src/components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="imaginify"
+      uploadPreset="priyanshu_pm"
       options={{
         multiple: false,
         resourceType: "image",
